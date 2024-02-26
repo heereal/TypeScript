8e119c5356263334975a82290ac7da4523792c0c
heeryoung <winterbooks@naver.com>
2024-02-26 16:23:03 +0900

    operators

---
+++ Basics/06_Operators.ts
@@ -0,0 +1,19 @@
+// Arithmetic Operators
+const sum: number = 5 + 3; // 8
+const difference: number = 8 - 5; // 3
+const product: number = 4 * 7; // 28
+const quotient: number = 20 / 4; // 5
+const remainder: number = 11 % 3; // 2
+
+// Logical Operators
+const isHome = true;
+const inPajamas = false;
+const wantsToEat = false;
+const isBored = false;
+
+const shouldGoOutside = isHome && !inPajamas && (wantsToEat || isBored);
+
+// Comparison Operators
+const number = 5;
+const between = number > 5 && number < 7;
+
