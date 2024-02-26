71504a4d61759172082a020d001cd7a0af8374b2
heeryoung <winterbooks@naver.com>
2024-02-26 17:14:16 +0900

    modules

---
+++ Basics/10_Modules.ts
@@ -0,0 +1,7 @@
+// Once this module is defined, you can use it from another module
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+// TypeScript allows you to name imports.
+// import { add as notMyAdd } from "./math";
