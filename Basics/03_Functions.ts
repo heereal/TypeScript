6c151a484c62cec1884aa259bae145e8cb7aec2f
heeryoung <winterbooks@naver.com>
2024-02-26 14:38:15 +0900

    functions

---
+++ Basics/03_Functions.ts
@@ -0,0 +1,7 @@
+// The function defines the types of the parameters and its return value using the : operator.
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Arrow functions also support a shorter form.
+const multiply = (a: number, b: number): number => a * b;
